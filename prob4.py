f90e7c15a15ad3bacc4c0a5f7ea7677512a9b4ef
Jesse S (Laptop) <jelloeater@gmail.com>
2014-06-06 19:54:17 -0400

    Head + Wall

--- prob4.py
+++ prob4.py
@@ -4,32 +4,48 @@ __author__ = 'Jesse'
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
+def printOnlyAnswers(list):
+	answers = []
+	for i in list:
+		answers.append(i[2])
+	answers.sort()
+	print(answers)
+	return answers
 
-def listPals(start = 100, stop = 200):
+
+def listPals(start=100, stop=200):
 	numRange = range(start, stop)
 
 	num1 = []
 	num2 = []
 	ans = []
+
+	def inAnsList(num):
+		for i in ans:
+			if i == num:
+				return True
+			else:
+				return False
+
 	for i in numRange:
 		for i2 in numRange:
 			x = i * i2
 			num = str(x)
 			if num[0] == num[-1] and num[1] == num[-2]:
-				if True: # FIXME
-					## TODO Check x against answers in ans
-					num1.append(i)
-					num2.append(i2)
-					ans.append(x)
-	listOutput = list(zip(num1,num2,ans))
+				# FIXME
+				# # TODO Check x against answers in ans
+				num1.append(i)
+				num2.append(i2)
+				ans.append(x)
 
 
-	answers = []
-	for i in listOutput:
-		answers.append(i[2])
+	listOutput = list(zip(num1, num2, ans))
 
-	answers.sort()
-	print(answers)
+	answers = printOnlyAnswers(listOutput)
+
+	for item in listOutput:
+		if item[1] != [answers in answers]:
+			print("DOUBLE")
 
 
 	listOutput = list(set(listOutput))
@@ -38,4 +54,4 @@ def listPals(start = 100, stop = 200):
 
 
 if __name__ == "__main__":  # Runs Script
-	listPals(5,55)
+	listPals(5, 55)
