5ba9a296bc007e251f4e17906c57d6bf675a4702
Jesse <jelloeater@gmail.com>
2014-06-06 21:43:22 -0400

    We can now count how many identical answers we have

--- prob4.py
+++ prob4.py
@@ -4,13 +4,23 @@ __author__ = 'Jesse'
 #
 # Find the largest palindrome made from the product of two 3-digit numbers.
 
-def printOnlyAnswers(list):
+
+def answerCountList(listIn):
 	answers = []
-	for i in list:
+	for i in listIn:
 		answers.append(i[2])
 	answers.sort()
-	print(answers)
-	return answers
+
+	answerNumbers = list(set(answers))
+	answerCount = []
+
+	for i in answers:
+		answerCount.append(answers.count(i))
+
+	answersAndCount = list(zip(answerNumbers, answerCount))
+
+	answersAndCount.sort(key=lambda answersAndCountIn: answersAndCountIn[0])
+	return answersAndCount
 
 
 def listPals(start=100, stop=200):
@@ -31,27 +41,36 @@ def listPals(start=100, stop=200):
 		for i2 in numRange:
 			x = i * i2
 			num = str(x)
-			if num[0] == num[-1] and num[1] == num[-2]:
+			if num[0] == num[-1] and num[1] == num[-2] and num[2] == num[-3]:
 				# FIXME
 				# # TODO Check x against answers in ans
 				num1.append(i)
 				num2.append(i2)
 				ans.append(x)
 
-
 	listOutput = list(zip(num1, num2, ans))
 
-	answers = printOnlyAnswers(listOutput)
+	print(answerCountList(listOutput))
+	print()
 
-	for item in listOutput:
-		if item[1] != [answers in answers]:
-			print("DOUBLE")
+	# # answers = list(set(answers))
+
+
+	# for listItem in listOutput:
+	# 	for i in answers:
+	# 			if listItem[2] == i:
+	# 				try:
+	# 					del listOutput[listOutput.index(listItem)]
+	# 				except:
+	# 					pass
 
 
 	listOutput = list(set(listOutput))
 
-	# print(listOutput)
+	listOutput.sort(key=lambda listOutputIN: listOutputIN[2])
+
+	print(listOutput)
 
 
 if __name__ == "__main__":  # Runs Script
-	listPals(5, 55)
+	listPals(700, 900)
