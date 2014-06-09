1c476f4c1e23140838a9c7508b31dbdc47f8072b
Jesse <jelloeater@gmail.com>
2014-06-08 21:57:49 -0400

    Found answer on stack exchange -_-

--- prob6.py
+++ prob6.py
@@ -1,8 +1,16 @@
 __author__ = 'Jesse'
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
-# What is the 10 001st prime number?
+# What is the 10001st prime number?
 
+import math
+def is_prime(n):
+	if n % 2 == 0 and n > 2:
+		return False
+	for i in range(3, int(math.sqrt(n)) + 1, 2):
+		if n % i == 0:
+			return False
+	return True
 
 def prime(x):
 	if str(x / 2).split(".")[1] != "0":  # Odd Number
@@ -27,7 +35,7 @@ def generateList(countTo=1000):
 	primeList = []
 	num = 1
 	while len(primeList) < countTo:
-		if prime(num):
+		if is_prime(num):
 			primeList.append(num)
 			print(str(len(primeList)) + " - " + str(num))
 		num += 1
@@ -35,5 +43,5 @@ def generateList(countTo=1000):
 	return primeList
 
 print("Answer")
-ansList = generateList(100)
+ansList = generateList(10001)
 print(ansList[-1])
