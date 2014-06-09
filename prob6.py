60db9ce8360a01065166d018aeba9e2793c47822
Jesse <jelloeater@gmail.com>
2014-06-08 20:41:10 -0400

    HUGE optimization

--- prob6.py
+++ prob6.py
@@ -2,6 +2,8 @@ __author__ = 'Jesse'
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # What is the 10 001st prime number?
+import timeit
+
 
 def prime(x):
 	if str(x / 2).split(".")[1] != "0":  # Odd Number
@@ -9,6 +11,10 @@ def prime(x):
 		numList = []
 
 		for numToCheck in range(x):
+			if divList.count(True) > 1:
+				break
+			# .0222 avg exec, vs .2325 wo @ 100 count
+			# 5.59 avg exec, vs 50.65 wo @ 1000 count O_O
 			if str(x / (numToCheck + 1)).split(".")[1] != "0":
 				divList.append(False)
 			else:
@@ -25,13 +31,22 @@ def prime(x):
 		return True
 
 
-primeList = []
-num = 1
-while len(primeList) < 10001:
-	if prime(num):
-		primeList.append(num)
-		print(str(len(primeList)) + " - " + str(num))
-	num += 1
+def generateList(countTo=1000):
+	primeList = []
+	num = 1
+	while len(primeList) < countTo:
+		if prime(num):
+			primeList.append(num)
+			# print(str(len(primeList)) + " - " + str(num))
+		num += 1
 
-print("Answer")
-print(primeList[-1])
+	return primeList
+
+# print("Answer")
+# print(generateList(100)[-1])
+times = []
+numToRun = 2
+for i in range(numToRun):
+	times.append(timeit.timeit(generateList, number=1))
+
+print(sum(times) / numToRun)
