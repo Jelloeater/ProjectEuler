34dc335010f21b6e8bfe05df7f9cd9ebc2f57c5e
Jesse <jelloeater@gmail.com>
2014-06-08 21:48:28 -0400

    Well it seems my little optimization is totally wrong, DOH!

--- prob6.py
+++ prob6.py
@@ -2,7 +2,6 @@ __author__ = 'Jesse'
 # By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
 #
 # What is the 10 001st prime number?
-import timeit
 
 
 def prime(x):
@@ -11,18 +10,11 @@ def prime(x):
 		numList = []
 
 		for numToCheck in range(x):
-			if divList.count(True) > 1:
-				break
-			# .0222 avg exec, vs .2325 wo @ 100 count
-			# 5.59 avg exec, vs 50.65 wo @ 1000 count O_O
 			if str(x / (numToCheck + 1)).split(".")[1] != "0":
 				divList.append(False)
 			else:
 				divList.append(True)
 			numList.append(numToCheck)
-
-		# pairList = list(zip(numList, divList))
-		# print([x for x in pairList if x[1] is True][1:])
 
 		if divList[0] is True and divList[-1:][0] is True:  # First and last should be divisible by themselves
 			for numToCheck in divList[1:-1]:  # Checks list to make sure all are False
@@ -37,16 +29,11 @@ def generateList(countTo=1000):
 	while len(primeList) < countTo:
 		if prime(num):
 			primeList.append(num)
-			# print(str(len(primeList)) + " - " + str(num))
+			print(str(len(primeList)) + " - " + str(num))
 		num += 1
 
 	return primeList
 
-# print("Answer")
-# print(generateList(100)[-1])
-times = []
-numToRun = 2
-for i in range(numToRun):
-	times.append(timeit.timeit(generateList, number=1))
-
-print(sum(times) / numToRun)
+print("Answer")
+ansList = generateList(100)
+print(ansList[-1])
